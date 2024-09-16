ab9eb393bee30e3e89175ba15ee0192c4c721926
AlphaNtihinduka <ntihindukaalpha@yahoo.com>
2024-09-16 11:56:43 +0300

    Created ArithmeticOperators class and operator instance

---
+++ src/ArithmeticOperators.java
@@ -0,0 +1,22 @@
+public class ArithmeticOperators {
+    public ArithmeticOperators() {
+
+    }
+    public int sum(int num1, int num2) {
+        return num1+num2;
+    }
+
+    public int sub(int num1, int num2) {
+        return num1-num2;
+    }
+
+    public int mul(int num1, int num2) {
+        return num1*num2;
+    }
+
+    public int div(int num1, int num2) {
+        return num1/num2;
+    }
+
+
+}
---
+++ src/ArithmeticTester.java
@@ -0,0 +1,12 @@
+public class ArithmeticTester {
+   public static void main(String[] args) {
+       ArithmeticOperators operator = new ArithmeticOperators();
+       System.out.println("Sum: "+operator.sum(80,10));
+       System.out.println("Sub: "+operator.sub(80,10));
+       System.out.println("Division: "+operator.div(80,10));
+       System.out.println("Multiplication: "+operator.mul(80,10));
+   }
+
+
+
+}
